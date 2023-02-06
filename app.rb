99f4f7c1f7016d7e5dcbc278755943a2752da16a
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-02-06 11:56:17 +0300

    Done with intro to ruby assignment

--- app.rb
+++ app.rb
@@ -1,23 +1,8 @@
-# pp [{ id: 1, hello: "World" }, 
-#     { id: 2, hello: "Ruby" }, 
-#     { id: 3, hello: "Moon" }, 
-#     { id: 4, hello: "Learner" }]
+#app.rb
+puts "Hello World!"
+print "Pass this test, please."
 
-# p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
+# This is the equivalent of calling:
+# puts [1, 2, 3].inspect
+p [1, 2, 3]
 
-# def add
-    
-# end
-# # 1 + 1
-# puts add + add
-
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
-
-puts add(1,3) + add(2,2)
